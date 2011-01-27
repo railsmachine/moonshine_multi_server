979cebd4cf9e228bc541fe0bbc7d07c3880b31d5
Lee Jones <scribblethink@gmail.com>
2011-01-27 12:23:16 -0500

    Install database gem in the app stack

--- lib/moonshine/multi_server.rb
+++ lib/moonshine/multi_server.rb
@@ -13,9 +13,9 @@ module Moonshine
     def default_db_stack
       case database_environment[:adapter]
       when 'mysql', 'mysql2'
-        recipe :mysql_server, :mysql_gem, :mysql_database, :mysql_user, :mysql_fixup_debian_start
+        recipe :mysql_server, :mysql_database, :mysql_user, :mysql_fixup_debian_start
       when 'postgresql'
-        recipe :postgresql_server, :postgresql_gem, :postgresql_user, :postgresql_database
+        recipe :postgresql_server, :postgresql_user, :postgresql_database
       when 'sqlite', 'sqlite3'
         recipe :sqlite3
       end
@@ -56,6 +56,12 @@ EOF
 
     def default_app_stack
       recipe :default_web_stack
+      case database_environment[:adapter]
+      when 'mysql', 'mysql2'
+        recipe :mysql_gem
+      when 'postgresql'
+        recipe :postgresql_gem
+      end
       recipe :passenger_gem, :passenger_configure_gem_path, :passenger_apache_module, :passenger_site
       recipe :rails_rake_environment, :rails_gems, :rails_directories, :rails_bootstrap, :rails_migrations, :rails_logrotate  
     end
