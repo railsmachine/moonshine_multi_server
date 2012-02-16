03c1f8aeeb8455a956d8c58974d7718bae48dbc0
Rails Machine Ops <ops@railsmachine.com>
2012-02-16 23:25:46 +0000

    Flesh out mongo recipe

--- generators/moonshine_multi_server/templates/configuration_builders.rb
+++ generators/moonshine_multi_server/templates/configuration_builders.rb
@@ -46,6 +46,31 @@ module ConfigurationBuilders
       raise 'FIXME needs implementation'
     end
 <% end %>
+<% if mongodb? %>
+    def build_mongodb_iptables_configuration
+      raise 'FIXME needs implementation'
+    end
+
+    def mongodb_replset
+      configuration[:application]
+    end
+
+    def build_mongodb_configuration
+      {
+        :replset => mongodb_replset,
+        :bind_ip => Facter.ipaddress_eth1
+      }
+    end
+
+    def build_mongodb_replset_initiate_config_object
+      members = mongodb_servers.map do |host|
+        id = host[:hostname].match(/mongo(\d+)\./)[1]
+        "{_id: #{id}, host: '#{host[:internal_ip]}'}"
+      end
+
+    "{_id: '#{mongodb_replset}', members: [#{members.join(', ')}]}"
+    end
+<% end %>
  
   end
 end
