56731eeb3db8aee40bb3f431978ccc0315d48e79
Rails Machine Ops <ops@railsmachine.com>
2012-02-16 23:25:11 +0000

    Fix app_manifest's constant

--- generators/moonshine_multi_server/templates/app_manifest.rb
+++ generators/moonshine_multi_server/templates/app_manifest.rb
@@ -1,5 +1,5 @@
 require "#{File.dirname(__FILE__)}/base_manifest.rb"
-class ApplicationManifest < BaseManifest
+class AppManifest < BaseManifest
   recipe :standalone_application_stack
 
   recipe :application_packages
