e9a83ba069e9599326fa1e02a79a951f05f55520
Josh Nichols <josh@technicalpickles.com>
2012-05-22 17:44:57 -0300

    Include fix for Ruby 1.9.

--- moonshine/init.rb
+++ moonshine/init.rb
@@ -1,4 +1,5 @@
-require "#{File.dirname(__FILE__)}/../lib/moonshine/multi_server.rb"
+$LOAD_PATH.unshift "#{File.dirname(__FILE__)}/../lib/"
+require 'moonshine/multi_server'
 
 # Instead of including the Moonshine::MultiServer module here, 
 # we need to include it in the manifests so that we can 
